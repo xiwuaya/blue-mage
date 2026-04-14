3ff5303dbe5f29f757ffbfc4ce701e129930b3bd
xiwuaya <xiwuaya@gmail.com>
2026-04-15 05:22:33 +0800

    v2.0

--- src/App.vue
+++ src/App.vue
@@ -116,7 +116,7 @@ const handleOrderChange = (val: boolean) => {
               <li><span class="color-def text-grey">灰色代表确定无法学会的途径，以免后人重复实验</span></li>
             </ul>
             <p>
-              本网页内容最近一次更新于<strong>2026年4月14日</strong>（7.45版本）。有对网页的建议反馈、或帮忙提供新的学习途径样本，可以<a href="https://docs.qq.com/sheet/DSE1BTnd5YkNJeGNk" target="_blank"
+              本网页内容最近一次更新于<strong>2026年4月15日</strong>（7.45版本）。有对网页的建议反馈、或帮忙提供新的学习途径样本，可以<a href="https://docs.qq.com/sheet/DSE1BTnd5YkNJeGNk" target="_blank"
                 rel="noopener noreferrer">点此提出</a>
             </p>
             <p>
