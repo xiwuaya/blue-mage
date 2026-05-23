2e9c5c93b409418a8984775807c023769f5bc94f
xiwuaya <xiwuaya@gmail.com>
2026-05-24 05:00:25 +0800

    v3.0

--- src/lib/interface.ts
+++ src/lib/interface.ts
@@ -4,7 +4,7 @@ export type SpellStatus = 0 | 1;
 export type SpellStatusArray = SpellStatus[];
 
 // 新增：专门为 UI 过滤器定义的分类键名（合并了 fate 和 treasure 为 other）
-export type FilterKey = "map" | "raid" | "dungeon" | "trail" | "special" | "other";
+export type FilterKey = "map" | "raid" | "dungeon" | "trail" | "carnivale" | "other";
 
 // 修改：FilterTypes 不再使用 Record<SpellType, boolean>
 export type FilterTypes = Record<FilterKey, boolean>;
